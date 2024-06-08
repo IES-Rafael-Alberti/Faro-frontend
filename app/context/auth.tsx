8c7bf83eb8844d5f36c659dcbbfa865f2cc6f271
JuanjoSG5 <113671548+JuanjoSG5@users.noreply.github.com>
2024-06-08 12:00:07 +0200

    fix: fixed an issue on how the added data is managed

--- app/context/auth.tsx
+++ app/context/auth.tsx
@@ -21,7 +21,7 @@ export const AuthContext = createContext<AuthContextType>({
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [isLogged, setIsLogged] = useState(true)
-  const [token, setToken] = useState<string>('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZlYmQ3MGY0LWZiNmUtNDM1Zi1hNWZhLTRjY2QyODJhMDk1NCIsImVtYWlsIjoianVhbnNoemc0NGwyNDNlejU1MTExQGdtYWlsLmNvbSIsImlhdCI6MTcxNzgzNjExNCwiZXhwIjoxNzE3ODM5NzE0fQ.cQ9GLN1ndQRPWVtmalwfNwk8dj41hMTSaHWKXrvLwiE')
+  const [token, setToken] = useState<string>('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZlYmQ3MGY0LWZiNmUtNDM1Zi1hNWZhLTRjY2QyODJhMDk1NCIsImVtYWlsIjoianVhbnNoemc0NGwyNDNlejU1MTExQGdtYWlsLmNvbSIsImlhdCI6MTcxNzg0MDY0NiwiZXhwIjoxNzE3ODQ0MjQ2fQ.C1jS4pGTbsgRPYhtIDbj35Z2I2qd6jp9vXm5sqVesdc')
   const [id, setId] = useState<string>('6ebd70f4-fb6e-435f-a5fa-4ccd282a0954')
 
   return (
