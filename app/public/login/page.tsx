d1037690f1a8166d677b909db07f75e97e317c9c
F0rno <pfornell@iesrafaelalberti.es>
2024-06-11 23:17:35 +0200

    fix: cant type in login

--- app/public/login/page.tsx
+++ app/public/login/page.tsx
@@ -1,44 +1,27 @@
-// pages/login.js
-
-'use client'
-
-import { useState, useContext } from "react";
-import { useRouter } from "next/navigation";
+"use client";
 import styles from "./page.module.css";
 import Button from "@/components/buttons/button";
 import { montserrat } from "@/app/ui/fonts";
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useContext } from "react";
 import { authPost } from "@/utils/authApi";
 import { AuthContext } from "@/app/context/auth";
-import AuthFormSection from "@/components/shared/AuthFormSection";
-import FormHeader from "@/components/shared/FormHeader";
-import AuthInfoAside from "@/components/shared/AuthInfoAside";
+import { useRouter } from 'next/navigation';
 
-/**
- * 
- * This component provides a login form that allows users to authenticate into the application.
- * 
- * @returns {JSX.Element} - The JSX element representing the login page.
- */
-  const Login = () : JSX.Element => {
+export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState('');
   const { setToken, setIsLogged, setId } = useContext(AuthContext);
   const router = useRouter();
 
-  /**
-   * Function to handle form submission for login.
-   * 
-   * @param {Event} e - The form submission event.
-   */
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    const data = { email: email, password: password }; // Login data.
+    const data = { email: email, password: password };
 
     try {
-      const response = await authPost("auth/signIn", data); // Sending login request.
+      const response = await authPost("auth/signIn", data);
       if (response && response.access_token && response.id) {
         setToken(response.access_token);
         setIsLogged(true);
@@ -46,7 +29,7 @@ import AuthInfoAside from "@/components/shared/AuthInfoAside";
         router.push('/private/feed');
       }
     } catch (e) {
-      setError("Credenciales inválidas."); // Handling login errors.
+      setError("Credenciales inválidas");
     }
   };
 
@@ -54,27 +37,30 @@ import AuthInfoAside from "@/components/shared/AuthInfoAside";
     <main className={styles.wrapper}>
       <section className={styles.authContainer}>
         <article className={styles.authForm}>
-          <FormHeader title="Conéctese" subtitle="a su cuenta" styles={styles} />
+          <h1 className={styles.authTitle}>Conéctese</h1>
+          <h2 className={styles.authSubtitle}>a su cuenta</h2>
           <form className={styles.loginForm} onSubmit={handleSubmit}>
-            <AuthFormSection
-              type="text"
+            <input
+              className={[styles.authInput, montserrat.className, "antialiased"].join(' ')}
+              type="email"
+              id="email"
               name="email"
               placeholder="email"
+              aria-label="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              styles={styles}
-              montserratClassName={montserrat.className}
-              hasOutsideDiv={false}
+              required
             />
-            <AuthFormSection
+            <input
+              className={[styles.authInput, montserrat.className, "antialiased"].join(' ')}
               type="password"
+              id="password"
               name="password"
               placeholder="contraseña"
+              aria-label="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              styles={styles}
-              montserratClassName={montserrat.className}
-              hasOutsideDiv={false}
+              required
             />
             {error && <small className={styles.error}>{error}</small>}
             <Link className={styles.linkToRegister} href="/public/register">¿Aún no tiene una cuenta? Regístrese</Link>
@@ -90,6 +76,4 @@ import AuthInfoAside from "@/components/shared/AuthInfoAside";
       <Image className={styles.logo} src="/imgs/logoFaro.png" alt="logoFaro.png" width={100} height={100} />
     </main>
   );
-};
-
-export default Login;
+}
